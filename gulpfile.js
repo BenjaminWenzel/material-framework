f99e3ba4db95c8d2207c01bc280c78f45126c20e
Benjamin Wenzel <benjamin.wenzel@hartwig-at.de>
2016-01-25 20:34:33 +0100

    [TASK] Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var concat = require("gulp-concat");
+var cssnano = require("gulp-cssnano");
+var autoprefixer = require("gulp-autoprefixer");
+var rename = require("gulp-rename");
+var order = require("gulp-order");
+var uglify = require("gulp-uglify");
+var cached = require("gulp-cached");
+var remember = require("gulp-remember");
+var sourcemaps = require("gulp-sourcemaps");
+var vinylPaths = require("vinyl-paths");
+var del = require("del");
+var plumber = require("gulp-plumber");
+
+function cleanCss() {
+    return gulp
+        .src("*.css", {cwd: "css"})
+        .pipe(vinylPaths(del));
+}
+
+function buildCss() {
+    gulp.src(
+        [
+            "src/scss/material-framework.scss"
+        ])
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(sourcemaps.init())
+        .pipe(concat("material-framework.css"))
+        .pipe(cssnano({zindex: false}))
+        .pipe(rename({extname: ".min.css"}))
+        .pipe(sourcemaps.write("maps"))
+        .pipe(gulp.dest("css"));
+}
+
+function cleanJs() {
+    return gulp
+        .src("*.js", {cwd: "js"})
+        .pipe(vinylPaths(del));
+}
+
+function buildJs() {
+    gulp.src(
+        [
+            "src/js/material-framework.js",
+            "src/js/**/*.js"
+        ])
+        .pipe(plumber())
+        .pipe(cached("js"))
+        .pipe(sourcemaps.init())
+        .pipe(order(
+            [
+                "src/js/material-framework.js",
+                "src/js/**/*.js"
+            ]))
+        .pipe(uglify())
+        .pipe(remember("js"))
+        .pipe(concat("material-framework.js", {newLine: ";"}))
+        .pipe(rename({extname: ".min.js"}))
+        .pipe(sourcemaps.write("maps"))
+        .pipe(gulp.dest("js"));
+}
+
+function customWatchers() {
+    gulp.watch(
+        "src/scss/**/*.scss",
+        ["css"]
+    );
+    gulp.watch(
+        "src/js/**/*.js",
+        ["js"]
+    );
+}
+
+gulp.task("clean:css", cleanCss);
+gulp.task("css", ["clean:css"], buildCss);
+gulp.task("clean:js", cleanJs);
+gulp.task("js", ["clean:js"], buildJs);
+gulp.task("watch", ["default"], customWatchers);
+gulp.task("default", ["css", "js"]);
